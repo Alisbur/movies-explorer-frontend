35f92197f0a108b895c5a73cdd8c35c2e6c3a72b
Alisbur <mail-4-eugen@yandex.ru>
2023-07-09 18:31:38 +0300

    лендинг попал в pixelperfect

--- src/components/Tech/Tech.jsx
+++ src/components/Tech/Tech.jsx
@@ -4,7 +4,7 @@ function Tech() {
   return (
     <section className="tech">
       <div className="tech__content">
-        <h2 className="tech__title">О проекте</h2>
+        <h2 className="tech__title">Технологии</h2>
         <h3 className="tech__text-title">7 технологий</h3>
         <p className="tech__text">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
         <ul className="tech__items">
