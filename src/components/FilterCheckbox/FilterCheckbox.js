e79a41f7c949e2bc6214a92f4b5a9f7a7fbd22f5
Alisbur <mail-4-eugen@yandex.ru>
2023-07-20 16:37:19 +0300

    вылечил бэм после второго ревью

--- src/components/FilterCheckbox/FilterCheckbox.js
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -2,11 +2,13 @@ import './FilterCheckbox.css';
 
 function FilterCheckbox() {
   return (
-    <label id="chk-label" className="filter-checkbox__label button-transparency" aria-label="фильтр короткометражных фильмов">
-    Короткометражки
-      <input type="checkbox" className="filter-checkbox__checkbox_type_hidden" />
-      <span className="filter-checkbox__checkbox_type_custom" role="checkbox" aria-checked="false" tabIndex="0" aria-labelledby="chk-label"/>
-    </label>
+    <div className="filter-checkbox">
+      <label id="chk-label" className="filter-checkbox__label button-transparency" aria-label="фильтр короткометражных фильмов">
+      Короткометражки
+        <input type="checkbox" className="filter-checkbox__checkbox-hidden" />
+        <span className="filter-checkbox__checkbox-custom" role="checkbox" aria-checked="false" tabIndex="0" aria-labelledby="chk-label"/>
+      </label>
+    </div>
   );
 }
 
